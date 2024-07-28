b047da1f71c91e49c3ecdc81b50002cdda9745a5
José David García Rodríguez <josdagaro@gmail.com>
2024-07-28 04:16:08 -0500

    Add health path

--- api.py
+++ api.py
@@ -70,5 +70,11 @@ def get_clientes():
     conn.close()
     return jsonify(clientes)
 
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return '', 200
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
